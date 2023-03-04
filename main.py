3797ab6c3b0c5e11743849cf3b5adae3be9d16b0
Itihas verma <itihask56@gmail.com>
2023-03-04 15:19:58 +0530

    "third commit"

--- main.py
+++ main.py
@@ -37,8 +37,9 @@ def generate_password():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
 def save():
-    website = website_input.get()
+    website = website_input.get().lower()
     email = email_input.get()
     password = password_input.get()
     new_data = {
@@ -51,9 +52,7 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message=f"Please make sure that you haven't left any field empty! ")
     else:
-        # is_ok = messagebox.askokcancel(title=website,
-        #                                message=f"These are the details you have entered: Website:{website}\n "
-        #                                        f"Email:{email}\nPassword:"f"{password}\nIs this ok to save?")
+
         try:
             with open("data.json", 'r') as data_file:
                 # data_file.write(f"{website} | {email} | {password}\n")
@@ -65,14 +64,34 @@ def save():
             with open("data.json", 'w') as data_file:
                 # Saving updated data
                 json.dump(new_data, data_file, indent=4)
+
         else:
             data.update(new_data)
-
-            with open("data.json",'w') as data_file:
+            with open("data.json", 'w') as data_file:
                 json.dump(data, data_file, indent=4)
+
         finally:
             website_input.delete(0, END)
             password_input.delete(0, END)
+
+
+# ----------------------------SEARCH PASSWORD ------------------------- #
+def search_password():
+    website = website_input.get().lower()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    except KeyError:
+        messagebox.showinfo(title="Error", message="No Website Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email:{email}\n Password:{password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists. ")
 
 
 
@@ -88,9 +107,9 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website: ")
 website_label.grid(row=1, column=0)
-website_input = Entry(width=39)
+website_input = Entry(width=22)
 website_input.focus()
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
@@ -102,6 +121,9 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 password_input = Entry(width=22)
 password_input.grid(row=3, column=1)
+
+search_button = Button(text="Search", width=13,command=search_password)
+search_button.grid(row=1, column=2)
 
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
